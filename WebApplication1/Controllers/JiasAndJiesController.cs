5e8e9f8036d3181e72cd4041abe54cf0ec5bfa4c
hoho-yin <343888569@qq.com>
2020-06-30 10:32:12 +1000

    layout

--- WebApplication1/Controllers/JiasAndJiesController.cs
+++ WebApplication1/Controllers/JiasAndJiesController.cs
@@ -8,12 +8,21 @@ namespace WebApplication1.Controllers
 {
     public class JiasAndJiesController : Controller
     {
-        //Show
+        // Show
         // GET: JiasAndJies
         public ActionResult Index()
         {
-            return View();
+            HttpCookie cookie = System.Web.HttpContext.Current.Request.Cookies["Jia"];
+            if (cookie == null)
+            { return View(); }
+            else
+            {
+                Session["jiami"] = HttpUtility.UrlDecode(cookie.Values["JiaMi"]).ToString();
+                Session["jiemi"] = HttpUtility.UrlDecode(cookie.Values["JieMi"]).ToString();
+                return RedirectToAction("IndexJie", "JiasAndJies");
+            }
         }
+
         //Encode
         public ActionResult Indexget()
         {
@@ -27,13 +36,20 @@ namespace WebApplication1.Controllers
             string JiaMi = Convert.ToString(Request.Form["JiaMi"]);
             if (JiaMi.Trim().Length > 0)
             {
-                Session["jiami"] = "Encoded Data: "+ Guid.NewGuid().ToString(); ;
-                Session["jiemi"] = JiaMi;
-                return Content("<script>alert('Encode data successfully 😊');window.open('" + Url.Action("Indexget", "JiasAndJies") + "', '_self')</script>");
+                HttpCookie cookie = new HttpCookie("Jia");
+                //Set Expire Time
+                cookie.Expires = DateTime.Now.AddDays(3);
+                //Save to client
+                System.Web.HttpContext.Current.Response.Cookies.Add(cookie);
+                cookie.Values["JieMi"] = HttpUtility.UrlEncode(JiaMi);
+                cookie.Values["JiaMi"] = HttpUtility.UrlEncode("Encoded data: " + Guid.NewGuid().ToString());
+                Session["jiami"] = HttpUtility.UrlDecode(cookie.Values["JiaMi"]).ToString();
+                Session["jiemi"] = HttpUtility.UrlDecode(cookie.Values["JieMi"]).ToString();
+                return Content("<script>alert('Encode successfully！');window.open('" + Url.Action("Indexget", "JiasAndJies") + "', '_self')</script>");
             }
             else
-                Session["jiami"] = "Sorry. Unable to encode data";
-                return Content("<script>alert('Sorry. Unable to encode data');window.open('" + Url.Action("Indexget", "JiasAndJies") + "', '_self')</script>");
+                Session["jiami"] = "Sorry. Unable to encode";
+                return Content("<script>alert('Sorry. Unable to encode');window.open('" + Url.Action("Indexget", "JiasAndJies") + "', '_self')</script>");
         }
 
 
